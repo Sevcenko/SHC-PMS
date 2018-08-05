e1906a76742c9e59eaa0cdbcdc5ad185756ff652
Deividas <pagerintas@gmx.com>
2018-08-05 19:58:02 +0000

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_doctor, :logged_in?
+
+  def current_doctor
+  	@current_doctor ||= Doctor.find(session[:doctor_id]) if session[:doctor_id]
+  end
+
+  def logged_in?
+  	!!current_doctor
+  end
+
+  def must_login
+  	if !logged_in?
+  		flash[:danger] = "Please login..."
+  		redirect_to login_path
+  	end
+  end
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,66 @@
 class PatientsController < ApplicationController
+	before_action :must_login
+	def index
+		if params[:search]
+			@patients = Patient.search(params[:search]).order("name")
+		else	
+			@patients = Patient.all
+			#@doctor = Doctor.find(params[:doctor_id])
+			#@patient = @patient.doctor.create(patient_params)
+			#redirect_to patient_path(@doctor)
+		end
+	end
+
+	def show
+		@patient = Patient.find(params[:id])
+	end
+
+	def new
+		@patient = Patient.new
+	end
+
+	def create
+		#render plain: params[:post].inspect
+		@patient = Patient.new(patient_params)
+		@patient.doctor_id = session[:doctor_id]
+
+	
+		if(@patient.save)
+		#sitas kasko neveikia
+		 redirect_to @patient
+		else 
+			render 'new'
+		end	
+	end
+
+	def edit
+		@patient = Patient.find(params[:id])
+		@patient.doctor_id = session[:doctor_id]
+
+	end
+
+	def update
+		@patient = Patient.find(params[:id])
+		@patient.doctor_id = session[:doctor_id]
+
+
+		if(@patient.update(patient_params))
+		#sitas kasko neveikia
+		 redirect_to @patient
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@patient = Patient.find(params[:id])
+		@patient.destroy
+
+		redirect_to patients_path
+	end
+
+
+	private def patient_params
+		params.require(:patient).permit(:name, :surname, :email, :birthday, :addressNumber, :address_street, :city, :phoneNumber, :clinic)
+	end
 end
